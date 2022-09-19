ffd8993bbe97405032f366208df3df12e734f20c
Rodrigues <Elaine.Rodrigues@libertymutual.com>
2022-09-19 15:05:19 -0400

    new changes

---
+++ assests/scripts/nationa_parks.js
@@ -0,0 +1,7 @@
+let filterParkLocation = document.querySelector("#filterByParkLocation")
+let filterParkType = document.querySelector("#filterByParkType")
+let filterViewAll = document.querySelector("#viewAllParks")
+let theLocationsUL = document.querySelector("#locations")
+let types = document.querySelector("#types")
+
+let searchTypeDDL = document.querySelector("#filterOptions");
